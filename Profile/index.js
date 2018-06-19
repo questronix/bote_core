155e1aa5b6098f2f7a3ea4f60bf203040640a0f6
jonlowi <jjmatienzo@up.edu.ph>
2018-06-19 16:56:08 +0800

    updated model and routes of profile

--- Profile/index.js
+++ Profile/index.js
@@ -2,16 +2,28 @@ const express = require('express');
 const router = express.Router();
 
 const mw = require('../common/middleware/Authentication');
-const pm = require('./model/Profile');
+const profile = require('./model/Profile');
+const followers = require('./model/Followers');
+const following = require('./model/Following');
 
-router.get('/', mw.isAuthenticated, (req, res)=>{
-  pm.getUserProfile(req.session.user.id)
-  .then(data=>{
-    res.json(data);
-  })
-  .catch(error=>{
-    res.status(400).json(error);
-  });
+router.get('/:username', mw.isAuthenticated, (req, res)=>{
+  if (req.params.username === req.session.user.username){
+    profile.getUserProfile(req.session.user.id)
+    .then(data=>{
+      res.json(data);
+    })
+    .catch(error=>{
+      res.status(400).json(error);
+    });
+  }else{
+    profile.getOtherProfile(req.params.username)
+    .then( data=> {
+      res.json(data);
+    })
+    .catch(error => {
+      res.json(error);
+    });
+  }
 });
 
 module.exports = router;
